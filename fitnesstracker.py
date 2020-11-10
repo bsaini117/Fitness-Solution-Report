ec8cdae1673618050a17356366e2a612ab26853d
Bhavesh Saini <bsaini117@gmail.com>
2020-11-10 14:42:23 -0500

    Just updated our code structure

---
+++ fitnesstracker.py
@@ -0,0 +1,41 @@
+class FitnessTracker:
+    
+    def __init__():
+        self.age
+        self.gender
+        self.estimation_formula
+        
+    def HowOften():
+        """
+        Definition:
+        Args:
+        Returns:
+        """
+        #Gain, maintain, or lose weight (goal)
+        
+    def CalorieCalculator():
+        """
+        Definition:
+        Args:
+        Returns:
+        """
+    
+    def WorkoutRegime():
+        #Three regimes depending on gain, maintain or lose
+        #Different interval based on three regimes
+        
+    def Diet():
+        #Macro ratios based on gaining, maintaining, or losing weight, along with the calories
+        #Ratio of calories you want to be protein, carbs, and fat
+        #Text file of diets
+        
+    def Cardio():
+        #Amount of cardio based on ratio
+        #One cardio workout per parameter
+        
+    def Cooldown():
+        #Three cooldown routines
+    
+    def Tips():
+        #Based on three options
+        
