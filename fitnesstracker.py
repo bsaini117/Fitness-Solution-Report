9ec8534ac3687a88516a925b157ae8b6d373f277
amiagnihotri <73482970+amiagnihotri@users.noreply.github.com>
2020-11-10 16:03:55 -0500

    Update fitnesstracker.py

--- fitnesstracker.py
+++ fitnesstracker.py
@@ -1,6 +1,12 @@
 class FitnessTracker:
-    
+    """ calls class FitnessTracker"""
     def __init__():
+        """Creates attributes and stores the information from the parameters 
+            Args: 
+                age(int): the number of the person's age
+                gender (str): the name of the gender
+                estimation formula: 
+        """
         self.age
         self.gender
         self.estimation_formula
@@ -15,9 +21,10 @@ class FitnessTracker:
         
     def CalorieCalculator():
         """
-        Definition:
+        Calculates the number of calories depending on the amount entered based on the calorie calculation.
         Args:
-        Returns:
+        Raises: ValueError: Raises a value error if user inputs a negative number.
+        Returns: the number of calories based on 
         """
     
     def WorkoutRegime():
@@ -38,4 +45,9 @@ class FitnessTracker:
     
     def Tips():
         #Based on three options
+        """ 
+        Args:   
+            Filename: the path to the file
+        Returns:
+            a quote from the opened file based on the three workout regimes"""
         
